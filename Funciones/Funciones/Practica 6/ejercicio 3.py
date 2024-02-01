ab778826271591d3426f65dd95a4075a402a06ad
MrIochy <157193100+MrIochy@users.noreply.github.com>
2024-01-31 20:32:32 -0600

    Se realizo el ejercicio 2 de la practica 6

--- Funciones/Funciones/Practica 6/ejercicio 3.py
+++ Funciones/Funciones/Practica 6/ejercicio 3.py
@@ -1,1 +1,9 @@
 #3. Escribir una función que convierta un número decimal en binario y otra que convierta un número binario en decimal.
+pregunta = int(input("Digite 1 para conversion de decimal a binario o 2 para conversion de binario a decimal"))
+if pregunta == 1:
+    def binarioadecimal(numero):
+        
+elif pregunta ==2:
+    
+else:
+    print("No has seleccionado ninguna opcion")
