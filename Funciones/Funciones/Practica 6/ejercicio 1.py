d0b17752baccd96d889561bfb5baeb573480fb2d
MrIochy <157193100+MrIochy@users.noreply.github.com>
2024-02-01 23:51:39 -0600

    Se finalizo la Practica 6

--- Funciones/Funciones/Practica 6/ejercicio 1.py
+++ Funciones/Funciones/Practica 6/ejercicio 1.py
@@ -4,3 +4,16 @@ el IVA. La función debe recibir la cantidad sin IVA y el porcentaje de
 IVA a aplicar, y devolver el total de la factura. Si se invoca la función
 sin pasarle el porcentaje de IVA, deberá aplicar un 21%.
 """
+cantidad=float(input("Digite la cantidad a aplicar IVA: "))
+IVA=input("Digite el porcentaje de IVA: ")
+if IVA.strip():
+    IVA=float(IVA)
+else:
+    IVA=21
+
+def factura(cantidad, IVA=21):
+    total = cantidad + cantidad*IVA/100
+    return total
+
+total = factura(cantidad, IVA)
+print("El total de la factura es: ", total)
--- Funciones/Funciones/Practica 6/ejercicio 3.py
+++ Funciones/Funciones/Practica 6/ejercicio 3.py
@@ -1,9 +1,22 @@
 #3. Escribir una función que convierta un número decimal en binario y otra que convierta un número binario en decimal.
-pregunta = int(input("Digite 1 para conversion de decimal a binario o 2 para conversion de binario a decimal"))
-if pregunta == 1:
-    def binarioadecimal(numero):
-        
-elif pregunta ==2:
-    
-else:
-    print("No has seleccionado ninguna opcion")
+numerobinario=int(input("Digite un numero para transformar a binario: "))
+numerodecimal=int(input("Digite un numero para transformar a decimal: "))
+
+def binariodecimal(numero):
+    numero = list(str(numero))
+    numero.reverse()
+    decimal = 0
+    for i in range (len(numero)):
+        decimal += int(numero[i]) *2 **i
+    return decimal
+
+def decimalbinario(numero):
+    binario =[]
+    while numero > 0:
+        binario.append(str(numero%2))
+        numero//=2
+    binario.reverse()
+    return "".join(binario)
+
+print(f"{numerobinario} en decimal: {decimalbinario(numerobinario)}")
+print(f"{numerodecimal} en binario: {binariodecimal(numerodecimal)}")
