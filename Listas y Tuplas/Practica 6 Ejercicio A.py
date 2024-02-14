39959667caca3b826e67122a6427287e38047739
MrIochy <157193100+MrIochy@users.noreply.github.com>
2024-02-13 23:43:40 -0600

    Practica 6 Terminada

--- Listas y Tuplas/Practica 6 Ejercicio A.py
+++ Listas y Tuplas/Practica 6 Ejercicio A.py
@@ -11,17 +11,87 @@ funcional
 conjunto de datos
 """
 #listas
-sumatoria=0
-print("Digite una serie de numeros")
-while True:
-    NumerosGuardados = input()
-    if not NumerosGuardados:
-        break
-    datos = tuple(map(int, NumerosGuardados.split( )))
-    for num in datos:
-        sumatoria += num
-        
-        
-print("1. Sumatoria de los elementos de la lista", sumatoria)
+print("Inserte los números separados por espacios para crear una lista:")
+numeros = [int(x) for x in input().split()]
+
+print("Seleccione una opción:")
+print("1. Sumatoria de los elementos de la lista")
+print("2. Numero mayor de la lista")
+print("3. Numero menor de la lista")
+print("4. Promedio")
+print("5. Moda")
+print("6. Rango")
+
+opcion = int(input("Ingrese el número de la opción deseada: "))
+
+if opcion == 1:
+    print("La sumatoria de los elementos de la lista es:", sum(numeros))
+    
+elif opcion == 2:
+    print("El número mayor de la lista es:", max(numeros))
+    
+elif opcion == 3:
+    print("El número menor de la lista es:", min(numeros))
+    
+elif opcion == 4:
+    print("El promedio de la lista es:", sum(numeros) / len(numeros))
+    
+elif opcion == 5:
+    frecuencias = {}
+    for num in numeros:
+        if num in frecuencias:
+            frecuencias[num] += 1
+        else:
+            frecuencias[num] = 1
+    moda_valor = max(frecuencias, key=frecuencias.get)
+    print("La moda de la lista es:", moda_valor)
+    
+elif opcion == 6:
+    print("El rango de la lista es:", max(numeros) - min(numeros))
+    
+else:
+    print("Opción no válida")
 
 #tuplas
+"""
+print("Inserte los números separados por espacios para crear una tupla:")
+numeros = tuple(int(x) for x in input().split())
+
+print("Seleccione una opción:")
+print("1. Sumatoria de los elementos de la lista")
+print("2. Numero mayor de la lista")
+print("3. Numero menor de la lista")
+print("4. Promedio")
+print("5. Moda")
+print("6. Rango")
+
+opcion = int(input("Ingrese el número de la opción deseada: "))
+
+if opcion == 1:
+    print("La sumatoria de los elementos de la lista es:", sum(numeros))
+    
+elif opcion == 2:
+    print("El número mayor de la lista es:", max(numeros))
+    
+elif opcion == 3:
+    print("El número menor de la lista es:", min(numeros))
+    
+elif opcion == 4:
+    print("El promedio de la lista es:", sum(numeros) / len(numeros))
+    
+elif opcion == 5:
+    frecuencias = {}
+    for num in numeros:
+        if num in frecuencias:
+            frecuencias[num] += 1
+        else:
+            frecuencias[num] = 1
+    moda_valor = max(frecuencias, key=frecuencias.get)
+    print("La moda de la lista es:", moda_valor)
+    
+elif opcion == 6:
+    print("El rango de la lista es:", max(numeros) - min(numeros))
+    
+else:
+    print("Opción no válida")
+    """
